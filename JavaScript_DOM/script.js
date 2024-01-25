c2af2a8110b1904b602788c74275f1239874d25d
TAUSIF AHMAD <115718023+tausif754@users.noreply.github.com>
2024-01-25 22:06:54 +0000

    Selecting elements using DOM

--- JavaScript_DOM/script.js
+++ JavaScript_DOM/script.js
@@ -1,2 +1,17 @@
+//getElementById
 let msg=documentgetElementById("message");
     console.log(msg);
+//getElementByName
+let btn=document.getElementByName("language");
+console.log(btn);
+//getElementByTagName
+let heading=document.getElementsByTagName("h1");
+console.log(heading);
+//getElementByClassName
+let headingSecond=document.getElementsByClassName("firstHeading");
+console.log(firstHeading);
+//quarySelector() and quarySelectorAll()
+let mess=document.quarySelector(".firstHeading");
+console.log(mess);
+let mess2=document.quarySelectorAll(".firstHeading");
+// console.log(mess2);
